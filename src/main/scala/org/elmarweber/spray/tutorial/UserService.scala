9d011b4e2fe407267104ab3de641b94a258e163f
Elmar Weber <info@elmarweber.org>
2013-11-07 00:22:50 +0100

    - added test - added error mapper

--- src/main/scala/org/elmarweber/spray/tutorial/UserService.scala
+++ src/main/scala/org/elmarweber/spray/tutorial/UserService.scala
@@ -5,15 +5,29 @@ import spray.http._
 import spray.httpx.SprayJsonSupport._
 import scala.concurrent.ExecutionContext
 import UserProtocol._
+import reactivemongo.core.commands.LastError
 
 trait UserService extends HttpService with UserDao {
   protected[this] implicit def exCtx: ExecutionContext
+
+  // TODO: use JSON format that includes text error message, etc.
+  private val lastErrorMapper : LastError => StatusCode = { _.ok match {
+    case true => StatusCodes.OK
+    case false => StatusCodes.InternalServerError
+  }}
 
   val userRoute = pathPrefix("user") {
     path("") {
       get {
         complete {
           findAll()
+        }
+      } ~
+      post {
+        entity(as[User]) { user =>
+          complete {
+            insert(user) map lastErrorMapper
+          }
         }
       }
     } ~
@@ -26,13 +40,13 @@ trait UserService extends HttpService with UserDao {
       put {
         entity(as[User]) { user =>
           complete {
-            update(user)
+            update(user) map lastErrorMapper
           }
         }
       } ~
       delete {
         complete {
-          remove(username)
+          remove(username) map lastErrorMapper
         }
       }
     }
--- src/test/scala/org/elmarweber/spray/tutorial/UserServiceTest.scala
+++ src/test/scala/org/elmarweber/spray/tutorial/UserServiceTest.scala
@@ -3,13 +3,28 @@ package org.elmarweber.spray.tutorial
 
 import spray.testkit._
 import org.specs2.mutable._
-import org.specs2.specification.Scope
+import spray.http._
+import spray.httpx.SprayJsonSupport._
+
+import org.elmarweber.spray.tutorial.UserProtocol._
 
 class UserServiceTest extends Specification with Specs2RouteTest {
   "User Service" should {
-    "list users" in new DefaultContext {
+    "list users with no data must return empty array" in new DefaultContext {
       Get("/user/") ~> userRoute ~> check {
         responseAs[String] mustEqual "[]"
+      }
+    }
+
+    "create user and retrieve user" in new DefaultContext {
+      val testUser = User("foo", "Mr foo", "key")
+
+      Post("/user/", testUser) ~> userRoute ~> check {
+        status mustEqual StatusCodes.OK
+      }
+
+      Get("/user/foo/") ~> userRoute ~> check {
+        responseAs[User] mustEqual testUser
       }
     }
   }
